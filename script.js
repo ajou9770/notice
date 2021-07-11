5092f85ab585d0cd23aac5e341e1dc5d12652be4
ajou9770 <76585539+ajou9770@users.noreply.github.com>
2021-07-11 21:49:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function handleSumbit(event) {
     }
   const show = localStorage.getItem(value);
   if (show === null) {
-    h1.innerText = "[직원에게 문의 바랍니다.]";
+    h1.innerText = "[조회되지 않네요😥 직원에게 문의 바랍니다.👌]";
   } else {
     h1.innerText =`[조합원번호(ID)는 ${show} 입니다]`;
   }
